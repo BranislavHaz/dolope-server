2282765b98d1ccb9cb9a159f8dbd1432853e9c68
Branislav Ház <haz.branislav@gmail.com>
2023-08-18 20:24:50 +0200

    Test new version

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 app.get("/", function (req, res) {
-  res.send("Ahoj svet!");
+  res.send("Ahoj svet! Toto je tvoj pohreb.");
 });
 app.listen(8080, function () {
   console.log("Príklad aplikácie počúva na porte 3000!");
