683151b4307944c4b93892221c152edd99b36f70
Branislav Ház <haz.branislav@gmail.com>
2023-08-21 21:29:42 +0200

    Test mysql2

--- connection.js
+++ connection.js
@@ -1,12 +1,30 @@
-const db = require("mysql2-promise")();
+const mysql = require("mysql2/promise");
 require("dotenv").config();
 
-db.configure({
+const options = {
   host: process.env.HOST,
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
   port: process.env.PORT,
-});
+};
 
-module.exports = db;
+const connection = async () => {
+  return await mysql.createConnection(options);
+};
+
+/* const db = {
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  port: process.env.PORT,
+};
+
+const query = async (sql, params) => {
+  const connection = await mysql.createConnection(db);
+  const [results] = await connection.execute(sql, params);
+  return results;
+}; */
+
+module.exports = connection;
--- helpers/nabykov/productsGetFromDB.js
+++ helpers/nabykov/productsGetFromDB.js
@@ -1,12 +1,16 @@
-const db = require("../../connection");
+const connection = require("../../connection");
 
 const getAllProductsFromDatabase = async () => {
-  try {
-    const [products] = await db.query(`SELECT * FROM products_nabykov`);
+  /*   try {
+    const products = await db.query(`SELECT * FROM products_nabykov`);
+    console.log(products);
     return products;
   } catch (error) {
     throw error;
-  }
+  } */
+  const conn = await connection();
+  const [products] = await conn.execute(`SELECT * FROM products_nabykov`);
+  return products;
 };
 
 module.exports = getAllProductsFromDatabase;
