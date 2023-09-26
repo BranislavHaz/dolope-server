9627e73bbc7dee321262b093069460e6a41ab2a4
Branislav Ház <haz.branislav@gmail.com>
2023-09-26 18:22:04 +0200

    Add availability condition

--- helpers/demos/getFromDB.js
+++ helpers/demos/getFromDB.js
@@ -1,19 +1,21 @@
 const pool = require("../../connectDB");
 
 const getAllProductsFromDB = async () => {
-  const queryText = "SELECT * FROM products_demos";
+  const queryText = "SELECT * FROM products_demos AND availability = true";
   const { rows } = await pool.query(queryText);
   return rows;
 };
 
 const get10thicknessProductsFromDB = async () => {
-  const queryText = "SELECT * FROM products_demos WHERE thickness = 10";
+  const queryText =
+    "SELECT * FROM products_demos WHERE thickness = 10 AND availability = true";
   const { rows } = await pool.query(queryText);
   return rows;
 };
 
 const get18thicknessProductsFromDB = async () => {
-  const queryText = "SELECT * FROM products_demos WHERE thickness = 18";
+  const queryText =
+    "SELECT * FROM products_demos WHERE thickness = 18 AND availability = true";
   const { rows } = await pool.query(queryText);
   return rows;
 };
--- helpers/nabykov/getFromDB.js
+++ helpers/nabykov/getFromDB.js
@@ -1,7 +1,7 @@
 const pool = require("../../connectDB");
 
 const getAllProducts = async () => {
-  const queryText = "SELECT * FROM products_nabykov";
+  const queryText = "SELECT * FROM products_nabykov WHERE availability = true";
   const { rows } = await pool.query(queryText);
   return rows;
 };
